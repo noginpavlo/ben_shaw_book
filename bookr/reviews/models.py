04af8837b167b6ac9c12f22df92172bbfc247100
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-18 15:17:33 +0200

    Added ManyToMany relationship to models.

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.db import models
 
 
@@ -10,11 +11,13 @@ class Publisher(models.Model):
 
 
 class Book(models.Model):
-    """A published book"""
+    """A published book."""
 
     title = models.CharField(max_length=70, help_text="The title of the book.")
     publication_date = models.DateField(verbose_name="Date the book was published.")
     isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    contributors = models.ManyToManyField("Contributor", through="BookContributor")
 
 
 class Contributor(models.Model):
@@ -23,3 +26,36 @@ class Contributor(models.Model):
     first_name = models.CharField(max_length=50, help_text="The contributor's first name.")
     last_name = models.CharField(max_length=50, help_text="The contributor's last name.")
     email = models.EmailField(help_text="The contact email for the contributor.")
+
+
+class BookContributor(models.Model):
+    """Many to many required table for Book and Contributor relationships."""
+
+    class ContributionRole(models.TextChoices):
+        AUTHOR = "AUTHOR", "Author"
+        CO_AUTHOR = "CO_AUTHOR", "Co-Author"
+        EDITOR = "EDITOR", "Editor"
+
+    book = models.ForeighKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeighKey(Contributor, on_delete=models.CASCADE)
+    role = models.CharField(
+        verbose_name="The role this contributor had in the book.",
+        choices=ContributionRole.choices,
+        max_length=20,
+    )
+
+
+class Review(models.Model):
+    """A model for book reviews from users."""
+
+    content = models.TextField(help_text="The review text")
+    rating = models.IntegerField(help_text="The rating the reviewer has given.")
+    date_created = models.DateTimeField(
+        auto_now_add=True, help_text="The date and time the review was created."
+    )
+    date_edited = models.DateTimeField(
+        null=True,
+        help_text="The date and time the review was edited.",
+    )
+    creator = models.ForeighKey(auth.get_user_model(), on_delete=models.CASCADE)
+    # here you will continue from page 91
