7caa0dd25b9a3dfd167eb135a5d20da0a520c3e2
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-18 12:06:35 +0200

    Created models for Book and Publisher. Made migrations.

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -4,6 +4,22 @@ from django.db import models
 class Publisher(models.Model):
     """A company that publishes books."""
 
-    name = models.CharField(max_length=150, help_text="The name of the publisher.")
+    name = models.CharField(max_length=50, help_text="The name of the publisher.")
     website = models.URLField(help_text="The publisher's website.")
     email = models.EmailField(help_text="The publisher's email address.")
+
+
+class Book(models.Model):
+    """A published book"""
+
+    title = models.CharField(max_length=70, help_text="The title of the book.")
+    publication_date = models.DateField(verbose_name="Date the book was published.")
+    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
+
+
+class Contributor(models.Model):
+    """A contributor to a Book, e.g. author, editor, co-author."""
+
+    first_name = models.CharField(max_length=50, help_text="The contributor's first name.")
+    last_name = models.CharField(max_length=50, help_text="The contributor's last name.")
+    email = models.EmailField(help_text="The contact email for the contributor.")
