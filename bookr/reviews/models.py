d0b7d4c6cf78107899b4696c670254747ca65a92
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-20 10:54:38 +0200

    Added __str__() methods to Publisher, Book and Contributor fields. Updated db with some manual inputed mock data.

--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -9,6 +9,9 @@ class Publisher(models.Model):
     website = models.URLField(help_text="The publisher's website.")
     email = models.EmailField(help_text="The publisher's email address.")
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     """A published book."""
@@ -16,8 +19,13 @@ class Book(models.Model):
     title = models.CharField(max_length=70, help_text="The title of the book.")
     publication_date = models.DateField(verbose_name="Date the book was published.")
     isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(
+        Publisher, default=1, on_delete=models.CASCADE
+    )  # fix unknown publisher later, def=1
     contributors = models.ManyToManyField("Contributor", through="BookContributor")
+
+    def __str__(self):
+        return self.title
 
 
 class Contributor(models.Model):
@@ -26,6 +34,9 @@ class Contributor(models.Model):
     first_name = models.CharField(max_length=50, help_text="The contributor's first name.")
     last_name = models.CharField(max_length=50, help_text="The contributor's last name.")
     email = models.EmailField(help_text="The contact email for the contributor.")
+
+    def __str__(self):
+        return self.last_name
 
 
 class BookContributor(models.Model):
@@ -36,8 +47,8 @@ class BookContributor(models.Model):
         CO_AUTHOR = "CO_AUTHOR", "Co-Author"
         EDITOR = "EDITOR", "Editor"
 
-    book = models.ForeighKey(Book, on_delete=models.CASCADE)
-    contributor = models.ForeighKey(Contributor, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
     role = models.CharField(
         verbose_name="The role this contributor had in the book.",
         choices=ContributionRole.choices,
@@ -57,5 +68,7 @@ class Review(models.Model):
         null=True,
         help_text="The date and time the review was edited.",
     )
-    creator = models.ForeighKey(auth.get_user_model(), on_delete=models.CASCADE)
-    # here you will continue from page 91
+    creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, help_text="The Book that this review is for."
+    )
