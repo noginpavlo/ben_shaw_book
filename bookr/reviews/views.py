bd232a17de630f8f8a2ea4e1c08b1914a9918b9f
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-24 12:30:41 +0200

    Created welcome_view() that is greting the user and retrieves the number of books that are present in the db

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 
-def index(request):
-    name = "Highlander"
-    return render(request, "base.html", {"name": name})
+def welcome_view(request):
+    message = (f"<html><h1>Welcome to Bookr!</h1>"
+               f"<p>{Book.objects.count()} books and counting!</p></html>")
+    return HttpResponse(message)
