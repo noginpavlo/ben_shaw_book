466dfca89c69adacb5968f368a0c44b3ff6f7696
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-24 13:39:33 +0200

    Created templates folder to stor all the tamplates in the app. Base dir at basic folder level. Created a welcome template and changed view in reviews app to navigate to this template.

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Book
 
 
 def welcome_view(request):
-    message = (f"<html><h1>Welcome to Bookr!</h1>"
-               f"<p>{Book.objects.count()} books and counting!</p></html>")
-    return HttpResponse(message)
+    return render(request, 'base.html')
