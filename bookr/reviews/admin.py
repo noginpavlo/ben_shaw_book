fdd8df5bc6c923a2def55ed87639e2738bf0b5c7
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-25 13:33:03 +0200

    Created superuser for django admin. Created mock users and a group with administrative priviliges: Help Desk User. Registered Publisher, Contributor, Book, BookContributor and Review models in reviews/admin.py to be ablt to access this data from admin site.

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from reviews.models import Book, BookContributor, Contributor, Publisher, Review
 
-# Register your models here.
+# Register your models here
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book)
+admin.site.register(BookContributor)
+admin.site.register(Review)
--- bookr/reviews/urls.py
+++ bookr/reviews/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name="welcome_view"),
-    path('books/', views.book_list, name="book_list"),
+    path("", views.index, name="welcome_view"),
+    path("books/", views.book_list, name="book_list"),
 ]
