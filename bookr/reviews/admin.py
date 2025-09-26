b64d6acf92b6e49d2f741e473d80d70de634c1e8
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-26 11:00:38 +0200

    Customized admin header, title and index_title in admin.py.

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from reviews.models import Book, BookContributor, Contributor, Publisher, Review
+
+
+class BookrAdminSite(AdminSite):
+    title_header = "Bookr Admin"
+    site_header = "Bookr administration"
+    index_title = "Bookr site admin"
+
+
+admin_site = BookrAdminSite(name="bookr")
+
 
 # Register your models here
 admin.site.register(Publisher)
