d96031bf899bf68bad4bbdd2fd11771dbec87190
highlander-95 <nogin.pavlo@lll.kpi.ua>
2025-09-26 11:17:00 +0200

    Updated url patterns for django customized website

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
+import reviews.views
+from django.contrib import admin_site
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('reviews.urls'))
+    path("admin/", admin_site.urls),
+    path("book-search", reviews.views.book_search),
+    path("", include("reviews.urls")),
 ]
